5bb2577336028c1af44c22e804897eca95ecb7b1
Joshua Varghese Thomji <69610717+josh1221wa@users.noreply.github.com>
2022-08-29 23:10:06 +0530

    Day 6 complete

---
+++ Day 6/Day_6_Project.py
@@ -0,0 +1,16 @@
+# Check out the reeborg.ca maze question at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not(at_goal()):
+    if right_is_clear():
+        turn_right()
+        move()
+    elif not(wall_in_front()):
+        move()
+    else:
+        turn_left()
